9de50018bc3ad3b95a32a87eda6b90b9a6a28a43
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-06-06 14:30:01 +0300

    Доюавил SQL БД

---
+++ ProductRepositore.sql
@@ -0,0 +1,5 @@
+INSERT INTO ProductRepository(Title, Manufacturer, Сategories, Description, Price, Count) 
+VALUES('Chees', 'Podoksha', 'Качокавалла', 'Тут описание', 190, 5),
+('Chees', 'Подносковье', 'Адыгейский', 'Тут описание', 100, 5),
+('Chees', 'Лебедева', 'Маскарпоне', 'Тут описание', 60, 5),
+('Оборудование', 'Podoksha', 'Лира', 'Тут описание', 1200, 5)
