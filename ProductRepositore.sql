0e556fe9da523674e3d97f5af70206d73d02db52
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-07-03 21:16:18 +0300

    Удалил лишнее

--- ProductRepositore.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ProductRepository(Title, Manufacturer, Сategories, Description, Price, Count) 
-VALUES('Chees', 'Podoksha', 'Качокавалла', 'Тут описание', 190, 5),
-('Chees', 'Подносковье', 'Адыгейский', 'Тут описание', 100, 5),
-('Chees', 'Лебедева', 'Маскарпоне', 'Тут описание', 60, 5),
-('Оборудование', 'Podoksha', 'Лира', 'Тут описание', 1200, 5)
